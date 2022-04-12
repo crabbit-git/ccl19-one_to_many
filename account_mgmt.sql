a6ca767546e5bbedebe2bf54d32bec65ee413405
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-12 14:22:52 +0100

    Basic relationships established in full

--- account_mgmt.sql
+++ account_mgmt.sql
@@ -1,18 +1,29 @@
-DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS account_types;
+DROP TABLE IF EXISTS customers;
 
-CREATE TABLE accounts (
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE account_types (
     id SERIAL PRIMARY KEY,
     account_type VARCHAR(255)
 );
 
-CREATE TABLE customers (
+CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    account_id INT REFERENCES accounts(id)
+    sort_code INT,
+    account_number INT UNIQUE,
+    account_type_id INT REFERENCES account_types(id),
+    customer_id INT REFERENCES customers(id)
 );
 
-INSERT INTO accounts (account_type) VALUES ('business');
-INSERT INTO customers (name, account_id) VALUES ('Bill', 1);
+INSERT INTO customers (name) VALUES ('Bill');
+INSERT INTO account_types (account_type) VALUES ('current');
+INSERT INTO account_types (account_type) VALUES ('instant access savings');
+INSERT INTO account_types (account_type) VALUES ('cash ISA');
+INSERT INTO accounts (sort_code, account_number, account_type_id, customer_id) VALUES (040004, 44448888, 1, 1);
 
-SELECT * FROM customers
+SELECT * FROM accounts WHERE customer_id = 1
